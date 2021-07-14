2bae59f0f54807ff70be458614d122505111f395
Logu98 <80382223+Logu98@users.noreply.github.com>
2021-07-15 00:22:39 +0530

    NUMBERS AND MATH

---
+++ exp3.rb
@@ -0,0 +1,10 @@
+puts 'i will now counts my chickens'
+puts "hens {2+3}"
+puts 'HEns #{2+3}'
+puts "HEns #{2+3}"
+puts "books = #{100/50+20}"
+puts "total number of students"
+puts 20+40+(80/2)*2
+puts 20<50
+puts "if 5 is greater or equal to 6#{5>=6}"
+puts "if 5 is less or equl to -2? #{5<=-2}"
