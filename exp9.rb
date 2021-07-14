35ce699196e12426a10d0d1d6d1b1a8f1d4f5e58
Logu98 <80382223+Logu98@users.noreply.github.com>
2021-07-15 02:58:38 +0530

    Escape Sequences

---
+++ exp9.rb
@@ -0,0 +1,15 @@
+tab_8bit = "\tI'm tabbed in."
+nxt_line = "hello \n world"
+make_it_single = "2\\4"
+
+data =
+"""list of item:
+\t * scale
+\t * pencil
+\t * paper\n\t* a4 sheet"""
+
+puts tab_8bit
+puts nxt_line
+puts make_it_single
+
+puts data
