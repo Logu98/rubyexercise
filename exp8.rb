3b49350c82f2eba65d2b4b5d8634fb66aa96e882
Logu98 <80382223+Logu98@users.noreply.github.com>
2021-07-15 02:15:50 +0530

    Printing, Printing, Printing

---
+++ exp8.rb
@@ -0,0 +1,11 @@
+country_name= "india,sri lanka,canada,rome"
+ipl_team = "csk\nmi\nrcb\nsrh\ndc\nkkr\nk11\nrr"
+
+puts "country_name: #{country_name}"
+puts "ipl_team: #{ipl_team}"
+
+puts %{
+Jaishankar meets Chinese FM, says any unilateral change along LAC not acceptable to India
+From Pawar to Gandhis: How to read Prashant Kishor's meetings
+Gaganyaan: Vikas engine tested for 3rd time, Elon Musk congratulates Isro
+}
