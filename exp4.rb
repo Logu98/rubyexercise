e36cd1883ea0bbd894bc699ce70f9f8cd6d24436
Logu98 <80382223+Logu98@users.noreply.github.com>
2021-07-15 00:52:37 +0530

    Variables and Names

---
+++ exp4.rb
@@ -0,0 +1,8 @@
+cric_players = 11
+cric_batsman = 4
+cric_all_rounder = 3
+cric_bowler = 4
+
+puts "total players for the team = #{cric_batsman+cric_all_rounder+cric_bowler} "
+
+puts "how players left after all rounders = #{cric_players-cric_all_rounder}"
