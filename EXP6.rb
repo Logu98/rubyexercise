34893a41d3ee3090a1f4cc0577dd2bb9c510bb6c
Logu98 <80382223+Logu98@users.noreply.github.com>
2021-07-15 01:31:10 +0530

    Strings and Text

---
+++ EXP6.rb
@@ -0,0 +1,13 @@
+types_of_classes = 3
+x="there are #{types_of_classes} types of classes in the school"
+puts x
+class1="first year"
+class2="second year"
+class3="third year"
+
+puts " in clg #{class1} and #{class2} are junior for #{class3}"
+
+w="hi"
+l=" guys"
+
+puts w+l
