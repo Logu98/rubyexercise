57fffbdd2b340113781f825df11407fd5647411c
Logu98 <80382223+Logu98@users.noreply.github.com>
2021-07-15 01:16:08 +0530

    More Variables and Printing

---
+++ exp5.rb
@@ -0,0 +1,12 @@
+my_name = "logesh"
+my_age = 35
+my_height = 5.4
+my_weight = 70
+my_eyes = 'black'
+my_teeth = 'White'
+my_hair = 'balck'
+
+puts "my name is #{my_name}"
+puts "my weight is #{my_weight}"
+
+puts "my eye colour is #{my_eyes}and my hair colour is #{my_hair}"
